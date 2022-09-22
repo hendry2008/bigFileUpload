75376e26b68c73c762a0ea4b23764b219974970d
hendry2008 <ruiqingzheng@hotmail.com>
2022-09-22 18:31:59 +0800

    浏览器在总进度完成后通知服务器合并分片， 服务器端获取分片， 在收到通知后合并分片

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // https://juejin.cn/post/6844904046436843527#heading-4
 const http = require('http')
 const multiparty = require('multiparty')
+const fs = require('fs')
+// const fs = require('fs-extra')
+const path = require('path')
+const UPLOAD_DIR = 'upload'
+
 
 const server = http.createServer()
+
+const uploadSessions = {}
 
 server.on('request', (req, res) => {
   res.setHeader('Access-control-allow-origin', '*')
@@ -15,7 +22,7 @@ server.on('request', (req, res) => {
 
   // console.log('req :>> ', req)
 
-  const url = req.url
+  const url = req.url.split('?')[0]
   const method = req.method.toLocaleLowerCase()
 
   if (method === 'get' && url === '/') {
@@ -37,13 +44,44 @@ server.on('request', (req, res) => {
         return
       }
 
-      fields
-      files
+      // fields 是客户端发送的formData append 拼装的数据
+      // const { index, name } = fields.name
+      const [name] = fields.name
+      const [index] = fields.index
+      const [uploadId] = fields.uploadId
+      const { path } = files.chunk[0]
+
+      if (!uploadSessions[uploadId]) uploadSessions[uploadId] = []
+      uploadSessions[uploadId][index] = path
+      // console.log('fields:', JSON.stringify(fields, null, 4), 'files:', JSON.stringify(files, null, 4))
+      // console.log(`index: ${index} - ${path}`)
     })
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    const data = { success: 'ok'  }
+    const data = { success: 'ok' }
     res.write(JSON.stringify(data))
+    res.end()
+    return
+  }
+
+  if (method === 'get' && url === '/merge') {
+    console.log('req.query :>> ', req)
+    // 从url 获取参数
+    const params = {}
+    const paramsString = req.url.includes('?') ? req.url.split('?')[1] : ''
+    const entries = paramsString.split('&')
+    entries.forEach((entry) => {
+      const [key, value] = entry.split('=')
+      params[key] = value
+    })
+
+    const { uploadId } = params
+    const chunks = uploadSessions[uploadId]
+    const originFileName = uploadId.split('|')[0]
+    mergeDataFileEnd(chunks, originFileName)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`merge, ${JSON.stringify(params)}`)
     res.end()
     return
   }
@@ -53,9 +91,45 @@ server.on('request', (req, res) => {
   res.write('no found!!!!')
   res.end()
 })
+/***
+ * @chunkPathList { String [] }  chunk 文件path 数组
+ * @distFile { String } 目标文件
+ * @description 合并, 逐个读取chunk 写入到目标文件的末尾 
+ */
+
+function mergeDataFileEnd(chunkPathList, distFile) {
+  const distFileFullPath = path.resolve(__dirname, UPLOAD_DIR, distFile)
+  const _writeStream = fs.createWriteStream(distFileFullPath)
+  let _currentChunkIndex = 0
+
+  const appendFileEnd = (currentChunkIndex, writeStream) => {
+    if (currentChunkIndex > chunkPathList.length - 1) return
+    const chunkPath = chunkPathList[currentChunkIndex]
+    const readStream = fs.createReadStream(chunkPath)
+    readStream.pipe(writeStream, { end: false })
+
+    readStream.on('error', (error) => {
+      console.log('readStream error :>> ', error)
+      writeStream.close()
+    })
+
+    readStream.on('end', () => {
+      if (currentChunkIndex === chunkPathList.length - 1) {
+        writeStream.end()
+        return
+      } else {
+        _currentChunkIndex += 1
+        appendFileEnd(_currentChunkIndex, writeStream)
+      }
+    })
+  }
+  appendFileEnd(_currentChunkIndex, _writeStream)
+}
+
+// 对同一个文件, 多次 createWriteStream 进行写入
 
 server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
+  socket.end(`HTTP/1.1 400 Bad Request, ${err}\r\n\r\n`)
 })
 
 server.listen(3002, () => console.log('Server listening on port 3002'))
